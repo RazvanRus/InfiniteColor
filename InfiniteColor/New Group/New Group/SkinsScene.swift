8d09f63c17d683bfce48a1812f21ffe5c651a64a
Rus Razvan <rusrazvan@Russ-MacBook-Air.local>
2018-02-01 15:07:31 +0200

    photos renamed

--- InfiniteColor/New Group/New Group/SkinsScene.swift
+++ InfiniteColor/New Group/New Group/SkinsScene.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2018 Rus Razvan. All rights reserved.
 //
 
+
+
 import SpriteKit
 
 class SkinsScene: SKScene {
@@ -15,6 +17,17 @@ class SkinsScene: SKScene {
     
     func initialize() {
         createOctogon()
+        createRandomColor()
+    }
+    
+    func createRandomColor() {
+        let color = SKSpriteNode()
+        color.size = CGSize(width: 95, height: 60)
+        color.position = CGPoint(x: 0, y: 0)
+        color.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        color.color = hexStringToUIColor(hex: OctogonService.shared.getRandomPart())
+        
+        self.addChild(color)
     }
     
     func createOctogon() {
@@ -39,4 +52,31 @@ class SkinsScene: SKScene {
             self.view?.presentScene(mainMenuScene, transition: SKTransition.crossFade(withDuration: TimeInterval(0.5)))
         }
     }
+}
+
+
+// MARK: extension for hex string manipulation to ui color
+extension SkinsScene {
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+
 }
--- InfiniteColor/Service/OctogonService.swift
+++ InfiniteColor/Service/OctogonService.swift
@@ -26,7 +26,7 @@ class OctogonService {
     var scaleValue: CGFloat = 1.3
     var spinningAngle: CGFloat = 2.5
         
-    var parts = ["blue","darkGreen","green","lightBlue","lightGreen","pink","red","yellow"]
+    var parts = ["1F5767","4C9376","6DE445","76CFE3","2746F6","EBF74E","EC428D","ED3833"]
     let rotationAngles: [CGFloat] = [3.14159,1.5708,0,4.71239,2.35619,0.785398,5.49779,3.92699]
     
     func increaseSpinningAngle() { if spinningAngle<3.4 {spinningAngle += 0.1} }
